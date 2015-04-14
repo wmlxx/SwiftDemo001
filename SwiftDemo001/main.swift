9167703dc29cb29f634ef424b23791e89eb2e041
whitemouse <whitemouse@192.168.0.102>
2015-04-14 23:10:28 +0800

    Initial Commit

---
+++ SwiftDemo001/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDemo001
+//
+//  Created by whitemouse on 15/4/14.
+//  Copyright (c) 2015年 whitemouse. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
